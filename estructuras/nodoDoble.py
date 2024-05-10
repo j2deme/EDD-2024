38326d8651408d4341dea80a8bab0296e9c8809b
Jaime Jesús Delgado Meraz <j2deme@gmail.com>
2024-05-10 16:27:32 -0600

    Agrega alias para nodos

--- estructuras/nodoDoble.py
+++ estructuras/nodoDoble.py
@@ -3,5 +3,9 @@ class NodoDoble:
         self.valor = valor
         self.siguiente = None
         self.anterior = None
+
+        self.izquierdo = self.siguiente
+        self.derecho = self.anterior
+
     def __str__(self):
         return str(self.valor)
